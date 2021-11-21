e6ab0e4a6ff0b05c788109284fe1b9e7875f7f09
hitruk <hitruk@mail.ru>
2021-11-21 11:53:50 +0300

    modified file bs4_doc.py and page_obj/parent_page, added class ElementPage

--- bs4_doc.py
+++ bs4_doc.py
@@ -3,16 +3,31 @@
 from bs4 import BeautifulSoup
 import requests
 from page_obj.parent_page import HttpQuery
+from page_obj.parent_page import ElementPage
 
 def http_query(url):
-    ''' requests a page '''
+    """ requests a page """
 
     query = HttpQuery(url)
     html  = query.get_html_page()
-    print(html)
-        
+    return html
+
+def get_page_element(html):
+    """ Get page elements and text  """
+
+    elements = ElementPage(html)
+    result = elements.parent_page_element()
+    print(result)
+    return result
+
+def save_data(result):
+    pass
+
     
 if __name__ == '__main__':
 
     url = 'https://www.crummy.com/software/BeautifulSoup/bs4/doc/'  
-    http_query(url)
+    html = http_query(url)
+    result = get_page_element(html)
+    # save_data(result)
+
--- page_obj/parent_page.py
+++ page_obj/parent_page.py
@@ -1,5 +1,6 @@
 
 import requests
+from bs4 import BeautifulSoup
 
 class HttpQuery:
 
@@ -14,3 +15,25 @@ class HttpQuery:
             return self.r.text
         else:
             print(r.status_code)
+
+class ElementPage:
+
+    def __init__(self, html):
+        self.html = html
+        self.soup = BeautifulSoup(self.html, 'lxml')
+
+    def parent_page_element(self):
+
+        abc = self.soup.find('div', class_='body').find_all('div', class_='section')
+        res_text = []
+        for row in abc:
+            if row != '':
+                div_p = row.find_all('p')
+                for row in div_p:
+                    try:
+                        text_p = row.text
+                    except:
+                        text_p != ''
+                    if text_p != '':
+                        res_text.append(text_p)
+        return res_text
