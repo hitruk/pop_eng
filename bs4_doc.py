c91ffc54f44bdb55840aa61a9d2772d6a6477dde
hitruk <hitruk@mail.ru>
2021-11-21 23:45:26 +0300

    modified file: bs4_doc.py

--- bs4_doc.py
+++ bs4_doc.py
@@ -1,9 +1,11 @@
 
-
 from bs4 import BeautifulSoup
 import requests
 from page_obj.parent_page import HttpQuery
 from page_obj.parent_page import ElementPage
+import os
+
+
 
 def http_query(url):
     """ requests a page """
@@ -12,22 +14,49 @@ def http_query(url):
     html  = query.get_html_page()
     return html
 
+def save_html(html):
+    """ Save html file """
+
+    with open('parent_page.html', 'w') as file:
+        file.write(html)
+
+def open_html():
+    """ Open and return html file """
+
+    with open('parent_page.html', 'r') as file:
+        html = file.read()
+    return html
+
 def get_page_element(html):
     """ Get page elements and text  """
 
     elements = ElementPage(html)
     result = elements.parent_page_element()
-    print(result)
+    #print(result)
     return result
 
 def save_data(result):
-    pass
+    """pass"""
+    
+    wow = []
+    for rows in result:
+        words = rows.split() 
+        for row in words:
+            wow.append(row)
+    print(wow)
 
     
 if __name__ == '__main__':
 
     url = 'https://www.crummy.com/software/BeautifulSoup/bs4/doc/'  
-    html = http_query(url)
+    
+    if os.path.exists('../pop_eng/parent_page.html'):
+        pass
+    else:
+        html = http_query(url)
+        save_html(html)
+
+    html = open_html()
     result = get_page_element(html)
-    # save_data(result)
+    save_data(result)
 
